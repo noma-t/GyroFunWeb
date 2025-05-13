3bc5ec452bb2820d2d5fa3940972260aa06a61ee
AomiVel <kaitoyoutube@icloud.com>
2025-05-13 15:33:08 +0900

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [orientation, setOrientation] = useState({
+        alpha: 0,
+        beta: 0,
+        gamma: 0
+    });
+    
+    useEffect(() => {
+        const handleOrientation = (event: DeviceOrientationEvent) => {
+            console.log(event.absolute)
+            setOrientation({
+                alpha: event.alpha || 0,
+                beta: event.beta || 0,
+                gamma: event.gamma || 0
+            });
+        }
+        window.addEventListener("deviceorientation", handleOrientation);
+        
+        return () => {
+            window.removeEventListener("deviceorientation", handleOrientation);
+        }
+    }, []);
+    return (
+        <>
+            {/*<header><h1>Header</h1></header>*/}
+            <main>
+                <h1>Device Orientations</h1>
+                <p>Alpha: {orientation.alpha}</p>
+                <p>Beta: {orientation.beta}</p>
+                <p>Gamma: {orientation.gamma}</p>
+            </main>
+            {/*<footer><h1>Footer</h1></footer>*/}
+        </>
+    );
 }
 
 export default App;
